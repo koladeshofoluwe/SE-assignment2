a15533702d46e6b3317e7b59a4a948ce46cd3caf
koladeshofoluwe <koladeshofoluwe@gmail.com>
2025-10-15 23:16:47 -0500

    Created the C++ File for the Task. Closes #3

---
+++ SE_CPP_FILE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+	string yourName;
+	cout << "Type Name:";
+	cin >> yourName;
+	cout << "Your Name is" + yourName <<endl;
+
+	return 0;
+}
